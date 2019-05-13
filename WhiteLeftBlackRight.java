6a1a92d7076b86436eb8a364b91fc865df7cb4e0
Yulia Volkova <yuulia.volkova@gmail.com>
2019-05-13 21:41:48 +0100

    create gradual color change

--- WhiteLeftBlackRight.java
+++ WhiteLeftBlackRight.java
@@ -2,18 +2,18 @@ import processing.core.PApplet;
 
 /**
  * Created by yuliav on 01/04/2019.
- *
+ * <p>
  * Write a program that changes the background color to white when the mouse is on the
  * left side of the screen, and to black when it is on the right side of the screen.
  * Next, write a program that gradually changes the background color as you move the mouse from left to right.
  */
-public class WhiteLeftBlackRight extends PApplet{
+public class WhiteLeftBlackRight extends PApplet {
     int r;
     int g;
     int b;
 
 
-    public void settings(){
+    public void settings() {
         size(500, 500);
 
     }
@@ -29,15 +29,43 @@ public class WhiteLeftBlackRight extends PApplet{
     }
 
     //infinite loop - runs 60 times per second (we can change frame rate by using frameRate() method)
-    public void draw(){
-        if(mouseX < 150){
-            background(255,255,255);
-        }else{
+    public void draw() {
+        if (mouseX < 5 && mouseX > 0) {
 
-            background(0,0,0);
+            background(255, 255, 255);
+        }
+        if (mouseX < 50 && mouseX > 5) {
+
+            background(255 - 32, 255 - 32, 255 - 32);
+        }
+        if (mouseX < 100 && mouseX > 50) {
+
+            background(255 - 64, 255 - 64, 255 - 64);
         }
 
+        if (mouseX < 150 && mouseX > 100) {
 
+            background(255 - 96, 255 - 96, 255 - 96);
+        }
+        if (mouseX < 200 && mouseX > 150) {
+
+            background(96, 96, 96);
+        }
+
+        if (mouseX < 250 && mouseX > 200) {
+
+            background(64, 64, 64);
+        }
+
+        if (mouseX < 270 && mouseX > 250) {
+
+            background(32, 32, 32);
+        }
+
+        if (mouseX < 300 && mouseX > 270) {
+
+            background(0, 0, 0);
+        }
     }
 
     public void keyPressed() {
