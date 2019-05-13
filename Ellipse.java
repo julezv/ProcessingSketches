73997e52bf417e8ef6ceb6eed766e20bd6cf9ed8
Yulia Volkova <yuulia.volkova@gmail.com>
2019-05-13 22:58:18 +0100

    random ellipses with transparency

--- Ellipse.java
+++ Ellipse.java
@@ -6,19 +6,20 @@ import processing.core.PApplet;
 public class Ellipse extends PApplet {
     int x;
     int y;
+
     public void setup() {
-      x = 20;
-      y = 30;
+        x = 20;
+        y = 30;
     }
-    public void settings(){
+
+    public void settings() {
         size(700, 700);
-
-
     }
 
-    public void draw(){
-        fill(random(255),random(255),random(255));
-        ellipse(random(700), random(700), random(10,30), random(5,20));
+    public void draw() {
+        noStroke();
+        fill(random(255), random(255), random(255), random(200));
+        ellipse(random(700), random(700), random(10, 300), random(5, 400));
     }
 
     public static void main(String... args) {
